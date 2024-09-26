570ee52d184bf5957c371fc583ad9ac5a77714c2
hamshad <hiromega18@gmail.com>
2024-09-26 18:41:24 +0530

    chore: added Expo Media Library

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,30 @@
+import React from 'react';
 import { Stack } from 'expo-router';
 import {} from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function Home() {
+  const [permissionResponse, requestPremission] = MediaLibrary.usePermissions();
+  const [localAssets, setLocalAssets] = React.useState<MediaLibrary.Asset[]>();
+
+  React.useEffect(() => {
+    if (permissionResponse?.status !== 'granted') {
+      requestPremission();
+    } else {
+      loadLocalAssets();
+    }
+  }, [permissionResponse])
+
+  const loadLocalAssets = async () => {
+    const assetPage: MediaLibrary.PagedInfo<MediaLibrary.Asset> = await MediaLibrary.getAssetsAsync();
+    console.log(JSON.stringify(assetPage, null, 2));
+
+    setLocalAssets(assetPage.assets);
+  }
+
   return (
     <>
       <Stack.Screen options={{ title: 'Photos' }} />
-
     </>
   );
 }

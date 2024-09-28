06acdaac1d0066f29e958ea87cfc62ba10190256
hamshad <hiromega18@gmail.com>
2024-09-28 15:58:38 +0530

    feat: added Pagination

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,10 @@ import * as MediaLibrary from 'expo-media-library';
 
 export default function Home() {
   const [permissionResponse, requestPremission] = MediaLibrary.usePermissions();
-  const [localAssets, setLocalAssets] = React.useState<MediaLibrary.Asset[]>();
+  const [localAssets, setLocalAssets] = React.useState<MediaLibrary.Asset[]>([]);
+  const [hasNextPage, setHasNextPage] = React.useState<boolean>(true);
+  const [endCursor, setEndCursor] = React.useState<MediaLibrary.AssetRef>();
+  const [isLoading, setIsLoading] = React.useState(false);
 
   React.useEffect(() => {
     if (permissionResponse?.status !== 'granted') {
@@ -16,10 +19,16 @@ export default function Home() {
   }, [permissionResponse])
 
   const loadLocalAssets = async () => {
-    const assetPage: MediaLibrary.PagedInfo<MediaLibrary.Asset> = await MediaLibrary.getAssetsAsync();
+    if (isLoading) return;
+    setIsLoading(true);
+    const assetPage: MediaLibrary.PagedInfo<MediaLibrary.Asset> = await MediaLibrary.getAssetsAsync({ after: endCursor });
     console.log(JSON.stringify(assetPage, null, 2));
 
-    setLocalAssets(assetPage.assets);
+    setLocalAssets(values => [...values, ...assetPage.assets]);
+
+    setHasNextPage(assetPage.hasNextPage);
+    setEndCursor(assetPage.endCursor);
+    setIsLoading(false);
   }
 
   return (
@@ -30,6 +39,9 @@ export default function Home() {
         numColumns={4}
         contentContainerClassName='gap-0.5'
         columnWrapperClassName='gap-0.5'
+        onEndReached={loadLocalAssets}
+        onEndReachedThreshold={1}
+        refreshing={isLoading}
         renderItem={({item}) => (
           <Image source={{uri: item.uri}} className='w-1/4 aspect-square'/>
         )}

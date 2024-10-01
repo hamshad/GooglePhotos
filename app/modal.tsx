b5bcdedb0e6488ff8b8bc061d5cf53c753d340f7
hamshad <hiromega18@gmail.com>
2024-10-01 18:59:02 +0530

    feat: added login and logout

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,13 +1,19 @@
+import { Link, router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Platform } from 'react-native';
+import { Button } from '~/components/Button';
 
 import { ScreenContent } from '~/components/ScreenContent';
+import { supabase } from '~/utils/supabase';
 
 export default function Modal() {
   return (
-    <>
-      <ScreenContent path="app/modal.tsx" title="Modal" />
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </>
+      <>
+        {/* <ScreenContent path="app/modal.tsx" title="Modal" /> */}
+        <Button className='m-4 bg-blue-400 rounded-2xl' title='Sign out' onPress={() => 
+          supabase.auth.signOut().then(() => router.back())
+        } />
+        <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      </>
   );
 }
